359e7eb5e4d553780787126630dd80e7ad81511f
Gleyson Sampaio <glysns@hotmail.com>
2022-12-14 10:55:59 -0300

    cucumber

---
+++ spring-boot/tests/spring-boot-cucumber-explorer/src/test/java/digytal/spring/explorer/SpringBootCucumberExplorerRunTests.java
@@ -0,0 +1,14 @@
+package digytal.spring.explorer;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features", plugin = {"pretty", "json:target/cucumber-report.json"})
+class SpringBootCucumberExplorerRunTests {
+
+
+}
