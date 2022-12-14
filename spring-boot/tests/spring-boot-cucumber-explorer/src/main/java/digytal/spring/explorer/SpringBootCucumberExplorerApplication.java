85cfa8a8e748c92c16c400a457dd01a7a95e202f
Gleyson Sampaio <glysns@hotmail.com>
2022-12-14 10:26:54 -0300

    criando o projeto

---
+++ spring-boot/tests/spring-boot-cucumber-explorer/src/main/java/digytal/spring/explorer/SpringBootCucumberExplorerApplication.java
@@ -0,0 +1,13 @@
+package digytal.spring.explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCucumberExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCucumberExplorerApplication.class, args);
+	}
+
+}
---
+++ spring-boot/tests/spring-boot-cucumber-explorer/src/test/java/digytal/spring/explorer/SpringBootCucumberExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package digytal.spring.explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCucumberExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
